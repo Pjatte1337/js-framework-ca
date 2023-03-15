70c2b8ce55e686ebdf2a0f8eb86cdc7374b11b38
Joakim Vanebo <Joakimvanebo1@gmail.com>
2023-03-15 21:00:44 +0100

    Added theme

---
+++ src/styles/theme.jsx
@@ -0,0 +1,23 @@
+import { ThemeProvider } from "styled-components";
+
+const elegant = {
+  color: {
+    highlight: "#4f4f4f",
+    support: "#e0e2e2",
+    neutral: "#fff",
+  },
+  break: {
+    medium: "900px",
+    small: "550px",
+  },
+  border: {
+    thickness: "1px",
+    type: "solid",
+  },
+};
+
+export const Theme = ({ children }) => (
+  <ThemeProvider theme={elegant}>{children}</ThemeProvider>
+);
+
+export default Theme;
