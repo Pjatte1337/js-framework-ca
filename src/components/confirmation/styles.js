42a25f49dd02cdc9bf52631cfcdbedc5e4798b92
Joakim Vanebo <Joakimvanebo1@gmail.com>
2023-03-21 22:29:59 +0100

    adding confirmation

---
+++ src/components/confirmation/styles.js
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+export const StyledFeedback = styled.div`
+  text-align: center;
+  h1 {
+    text-transform: uppercase;
+  }
+  p {
+    margin-bottom: 5rem;
+  }
+  a {
+    text-decoration: none;
+    text-transform: uppercase;
+    color: ${({ theme }) => theme.color.neutral};
+    background-color: ${({ theme }) => theme.color.highlight};
+    padding: 0.5rem 1rem;
+    text-align: center;
+    &:hover {
+      color: ${({ theme }) => theme.color.hover};
+      background-color: ${({ theme }) => theme.color.neutral};
+      border: ${({ theme }) =>
+      `${theme.border.thickness} ${theme.border.type} ${theme.color.highlight}`};
+    }
+  }
+`;
