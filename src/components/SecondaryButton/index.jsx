0780c52bc18b59c57d5033e73335c55fba7e6508
Joakim Vanebo <Joakimvanebo1@gmail.com>
2023-03-21 19:28:56 +0100

    deleted unused files

--- src/components/SecondaryButton/index.jsx
+++
@@ -1,7 +0,0 @@
-import { StyledSecondaryButton } from "./styles";
-
-export default function SecondaryButton({ text, onClick }) {
-  return (
-    <StyledSecondaryButton onClick={onClick}>{text}</StyledSecondaryButton>
-  );
-}
