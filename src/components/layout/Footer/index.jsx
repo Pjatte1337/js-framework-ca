e3373d7bd93b16e9e55c6c779150c4139cc7e239
Joakim Vanebo <Joakimvanebo1@gmail.com>
2023-03-21 19:10:29 +0100

    Restructures files and removing unused path

--- src/components/layout/Footer/index.jsx
+++ src/components/layout/Footer/index.jsx
@@ -1,21 +1,9 @@
 import { StyledFooter } from "./styles";
-import { Link } from "react-router-dom";
 
 export default function Footer() {
   return (
     <StyledFooter>
       <h4>&copy; Joakim Vanebo | Pjatte</h4>
-      <ul>
-        <li>
-          <Link to="https://github.com/pjatte1337">GitHub</Link>
-        </li>
-        <li>·</li>
-        <li>
-          <Link to="">
-            LinkedIn
-          </Link>
-        </li>
-      </ul>
     </StyledFooter>
   );
 }
