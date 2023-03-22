f9ba9436f51dbad093cd692531ab521f8b8a2378
Joakim Vanebo <Joakimvanebo1@gmail.com>
2023-03-22 21:55:47 +0100

    fixing typo

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,29 +2,29 @@ import React, { useEffect } from "react";
 import { StyledPage } from "../pageStyles";
 import ProductCard from "../../components/ProductCard";
 
-export default function Home({ products }) {
+function Home({ products }) {
   useEffect(() => {
     document.title = "StoreTown | Homepage";
   }, []);
 
+  const productCards = products.map((product) => (
+    <ProductCard
+      key={product.id}
+      id={product.id}
+      imageUrl={product.imageUrl}
+      title={product.title}
+      price={product.price}
+      discountedPrice={product.discountedPrice}
+      description={product.description}
+    />
+  ));
+
   return (
-    <>
-      <StyledPage>
-        <h1>All Products</h1>
-        {products.map((product) => {
-          return (
-            <ProductCard
-              key={product.id}
-              id={product.id}
-              imageUrl={product.imageUrl}
-              title={product.title}
-              price={product.price}
-              discountedPrice={product.discountedPrice}
-              description={product.description}
-            />
-          );
-        })}
-      </StyledPage>
-    </>
+    <StyledPage>
+      <h1>All Products</h1>
+      {productCards}
+    </StyledPage>
   );
 }
+
+export default Home;
--- src/utils/filter.js
+++ src/utils/filter.js
@@ -1,5 +1,7 @@
-export default function filterByTags(products, tags) {
-    return products.filter((product) => {
-      return tags.some((tag) => product.tags.includes(tag));
-    });
-  }
+function filterByTags(products, tags) {
+  return products.filter((product) =>
+    tags.some((tag) => product.tags.includes(tag))
+  );
+}
+
+export default filterByTags;
