7928c049bade74fed25db1d34a90d7fe6f03382e
Joakim Vanebo <Joakimvanebo1@gmail.com>
2023-03-26 14:26:49 +0200

    doing som cleanup

--- src/components/OrderSummary/index.jsx
+++ src/components/OrderSummary/index.jsx
@@ -9,19 +9,15 @@ const OrderSummary = ({ calcSubtotal, calcVAT, calcTotal, clearCart }) => {
     <StyledOrderSummary>
       <h3>Order summary</h3>
       <div className="price-summary">
-        <p>Subtotal</p>
+        <p>Product cost:</p>
         <p>{calcSubtotal()} NOK</p>
       </div>
       <div className="price-summary">
-        <p>Shipping</p>
-        <p>Free</p>
-      </div>
-      <div className="price-summary">
-        <p>VAT 25%</p>
+        <p>VAT 25%:</p>
         <p>{calcVAT()} NOK</p>
       </div>
       <div className="price-summary">
-        <p className="total">Total</p>
+        <p className="total">Total:</p>
         <p className="total">{calcTotal()} NOK</p>
       </div>
       <Link to={"/src/pages/Checkout"}>Checkout</Link>
